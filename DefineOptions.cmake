aa9b0da96927e957f6ed0b1b9d51b16db90ea2af
Andreas Schneider <asn@samba.org>
2018-09-02 11:18:09 +0200

    cmake: Improve checks for compiler flags
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- DefineOptions.cmake
+++ DefineOptions.cmake
@@ -1,2 +1,3 @@
 option(UNIT_TESTING "Build with unit tests" OFF)
 option(HELGRIND_TESTING "Run threaded unit tests with helgrind" OFF)
+option(PICKY_DEVELOPER "Build with picky developer flags" OFF)
