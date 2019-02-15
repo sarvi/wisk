9a9250a79143df88c3100e509e29c00536045b6b
Andreas Schneider <asn@samba.org>
2019-02-15 13:09:28 +0100

    cmake: Use GNUInstallDirs
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- socket_wrapper.pc.cmake
+++ socket_wrapper.pc.cmake
@@ -1,4 +1,4 @@
 Name: @PROJECT_NAME@
 Description: The socket_wrapper library
 Version: @PROJECT_VERSION@
-Libs: @LIB_INSTALL_DIR@/@SOCKET_WRAPPER_LIB@
+Libs: @CMAKE_INSTALL_LIBDIR@/@SOCKET_WRAPPER_LIB@
