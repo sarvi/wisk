b2e3500811eea48907cf7d273ccef5ee1126a5bf
Saravanan Shanmugham <sarvi@cisco.com>
2019-10-27 15:33:47 +0530

    Test code

--- tests/wisktestrun.sh
+++ tests/wisktestrun.sh
@@ -4,6 +4,25 @@ SCRIPT="$SCRIPT_DIR/$SCRIPT_NAME"
 WORKSPACE_DIR="$( dirname "$SCRIPT_DIR" )"
 SCRIPT_SHORT_NAME="${SCRIPT_NAME%.*}"
 
+
+tee2log() {
+    local log_path
+
+    [[ -n "${1:-}" ]] || { echo "Must supply log path" >&2; return 1; }
+    log_path="$1"
+
+    if [[ ! -f "$log_path" ]]; then
+        mkdir -p "$( dirname "$log_path" )"
+    fi
+
+    # tee to log
+    echo "exec > >( tee -a $log_path ) 2>&1" >&2
+    exec > >( tee -a "$log_path" ) 2>&1 || true
+}
+
+tee2log program_uut.log
+
+
 # LD_PRELOAD="$WORKSPACE_DIR/src/lib/libwisktrack.so"
 # LD_LIBRARY_PATH="$WORKSPACE_DIR/src/lib64:$WORKSPACE_DIR/src/lib"
 LD_LIBRARY_PATH="$WORKSPACE_DIR/src/lib32:$WORKSPACE_DIR/src/lib64"
